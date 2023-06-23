345bbc914705c9815f1151388feb9fb8889d0636
icaroQre <icaro.queiroz.reccanello2@gmail.com>
2023-06-22 21:38:43 -0300

    about section 1/2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image'
 import Header from './components/Header'
 import HomeSection from './components/HomeSection'
 import AboutSection from './components/AboutSection'
-import Construction from './components/Construction'
 
 export default function Home() {
   return (
     <main>
-      <Construction />
+      <Header />
+      <HomeSection />
+      <AboutSection />
     </main>
   )
 }
